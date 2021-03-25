93c76d38e7b14f56722a7bdf71de620932377902
RiouC <riou.christophe@protonmail.com>
2021-03-25 16:02:37 +0100

    [cat] multiple files

--- cat.js
+++ cat.js
@@ -5,26 +5,30 @@ const [,, ...cmdArgs] = process.argv;
 
 const bn = basename(process.argv[1], '.js');
 
-if (cmdArgs.length !== 1) {
-    console.log('Usage: node cat.js FILE');
+if (cmdArgs.length === 0) {
+    console.log('Usage: node cat.js FILE...');
     process.exit(1);
 }
 
-// check if the path exist
-if (!fs.existsSync(cmdArgs[0])) {    
-    console.log(`${bn}: cannot access ${cmdArgs[0]}: No such file or directory`);
+
+for (const file of cmdArgs) {
+    if (!fs.existsSync(file)) {
+	console.log(`${bn}: cannot access ${cmdArgs[0]}: No such file or directory`);
+	process.exit(1);
+    }
+
+    const stats = fs.statSync(file);
+    if (stats.isDirectory()) {
+	console.log(`${bn}: ${cmdArgs[0]}: Is a directory`);
+	process.exit(1);
+    }
+    else if (stats.isFile()) {
+	const fileContent = fs.readFileSync(file).toString();
+	console.log(fileContent);
+    } else {
+	console.log('Error');
+	process.exit(1);
+    }
 }
 
-const stats = fs.statSync(cmdArgs[0]);
-if (stats.isDirectory()) {
-    console.log(`${bn}: ${cmdArgs[0]}: Is a directory`);
-    process.exit(1);
-}
-else if (stats.isFile()) {
-    const fileContent = fs.readFileSync(cmdArgs[0]).toString();
-    console.log(fileContent);
-    process.exit(0);
-} else {
-    console.log('Error');
-    process.exit(1);
-}
+process.exit(0);
