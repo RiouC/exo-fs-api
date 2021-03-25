3da54a3128e36eb624efa2638670ec21d5e5f117
RiouC <riou.christophe@protonmail.com>
2021-03-25 16:14:19 +0100

    [append] add append.js

---
+++ append.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const { basename } = require('path');
+
+const [,, ...cmdArgs] = process.argv;
+const dest = cmdArgs[cmdArgs.length -1]
+
+const bn = basename(process.argv[1], '.js');
+
+if (cmdArgs.length < 2) {
+    console.log('Usage: node append.js SRC... DEST');
+    process.exit(1);
+}
+
+
+for (const src of cmdArgs.slice(0, -1)) {
+    if (!fs.existsSync(src)) {
+	console.log(`${bn}: cannot access ${cmdArgs[0]}: No such file or directory`);
+	process.exit(1);
+    }
+
+    const stats = fs.statSync(src);
+    if (stats.isDirectory()) {
+	console.log(`${bn}: ${cmdArgs[0]}: Is a directory`);
+	process.exit(1);
+    }
+    else if (stats.isFile()) {
+	fs.appendFileSync(dest, fs.readFileSync(src).toString());
+    } else {
+	console.log('Error');
+	process.exit(1);
+    }
+}
+
+process.exit(0);
